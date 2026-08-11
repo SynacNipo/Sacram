79248c42af92182b4d3761f71ddc7dfa5e7d309e
SynacNipo <synacnipo@users.noreply.github.com>
2026-08-12 02:35:40 +0800

    Fix: import kotlinx.coroutines.isActive

--- app/src/main/java/com/sacram/proxy/ProxyService.kt
+++ app/src/main/java/com/sacram/proxy/ProxyService.kt
@@ -21,6 +21,7 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.util.concurrent.atomic.AtomicBoolean
 
