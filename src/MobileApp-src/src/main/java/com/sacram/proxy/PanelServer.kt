964d15fedf803f123b4d8537d22e839e4256236f
SynacNipo <synacnipo@gmail.com>
2026-08-22 00:54:40 +0800

    [Trigger] fix: add missing BufferedInputStream import in PanelServer

--- src/MobileApp-src/src/main/java/com/sacram/proxy/PanelServer.kt
+++ src/MobileApp-src/src/main/java/com/sacram/proxy/PanelServer.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.io.InputStream
 import java.io.OutputStream
